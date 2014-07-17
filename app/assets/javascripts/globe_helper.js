24893f124626be3bece07315d1eedf59c0b30a48
cdbajorin <christopher.bajorin@gmail.com>
2014-07-17 00:03:36 -0400

    added helper functions for satellite arc paths. arcs are still clipping in front

--- app/assets/javascripts/globe_helper.js
+++ app/assets/javascripts/globe_helper.js
@@ -20,9 +20,37 @@ function randomLonLat() {
 }
 
 
-function zoomed(d){
-    translate = zoom.translate();
-    scale = zoom.scale();
+function getArcs(inclination, startLongitude){
 
+    var i1 = 90 - inclination,
+        i2 = 270 + inclination,
+        la1 = 0,
+        lo1 = startLongitude,
+        d = 180/360 * 40075,
+        R = 6371;
+
+    i1 = degToRad(i1);
+    i2 = degToRad(i2);
+    φ1 = degToRad(la1);
+    λ1 = degToRad(lo1);
+
+    var φ2 = Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(i1) );
+    var λ2 = λ1 + Math.atan2(Math.sin(i1)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2));
+    var λ2d = λ1 + Math.atan2(Math.sin(i2)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2));
+
+    array = [[radToDeg(λ2), radToDeg(φ2)], [startLongitude, 0], [radToDeg(λ2d), -radToDeg(φ2)]]
+    return makeGeoObject(array);
+};
+
+
+function makeGeoObject(positionArray) {
+        object = [{
+            "type":"Feature",
+            "geometry":{
+                "type":"LineString",
+                "coordinates": positionArray
+            }
+        }];
+        return object;
 
 }
