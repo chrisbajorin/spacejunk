7f47ed7a02a76d2ab8ff6b99bf73e5180ebe59fa
cdbajorin <christopher.bajorin@gmail.com>
2014-07-23 22:24:32 -0400

    added a math.random function with range parameters, used it to increase minimum star opacity to make them a bit brighter

--- app/assets/javascripts/globe_helper.js
+++ app/assets/javascripts/globe_helper.js
@@ -22,42 +22,55 @@ function createStars(number){
 
 // equirectangular projection formula for latitude/longitude from inclination and starting point
 // reversed the haversine formula.
-function getArcs(inclination, startLongitude){
-    var i1 = 90 - inclination,
-        i2 = 270 + inclination,
-        la1 = 0,
-        lo1 = startLongitude,
-        d = 180/360 * 40075,
+// function getArcs(inclination, startLongitude){
+//     var i1 = 90 - inclination,
+//         i2 = 270 + inclination,
+//         la1 = 0,
+//         lo1 = startLongitude,
+//         d = 180/360 * 40075,
+//         R = 6371;
+
+//     i1 = degToRad(i1);
+//     i2 = degToRad(i2);
+//     φ1 = degToRad(la1);
+//     λ1 = degToRad(lo1);
+
+//     var φ2 = Math.asin(Math.sin(d/R)*Math.cos(i1) );
+//     var λ2 = λ1 + Math.atan2(Math.sin(i1)*Math.sin(d/R), Math.cos(d/R));
+
+//     // var φ2 = Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(i1) );
+//     // var λ2 = λ1 + Math.atan2(Math.sin(i1)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2));
+//     var λ2d = λ1 + Math.atan2(Math.sin(i2)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2));
+
+//     array = [[radToDeg(λ2), radToDeg(φ2)], [startLongitude, 0], [radToDeg(λ2d), -radToDeg(φ2)]]
+//     return makeGeoObject(array);
+// };
+
+function getOffset(satellite){
+    var i1 = degToRad(-inclination),
+        d = satellite.adjustedRAAN/360 * 40075,
         R = 6371;
 
-    i1 = degToRad(i1);
-    i2 = degToRad(i2);
-    φ1 = degToRad(la1);
-    λ1 = degToRad(lo1);
-
-    var φ2 = Math.asin( Math.sin(φ1)*Math.cos(d/R) + Math.cos(φ1)*Math.sin(d/R)*Math.cos(i1) );
-    var λ2 = λ1 + Math.atan2(Math.sin(i1)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2));
-    var λ2d = λ1 + Math.atan2(Math.sin(i2)*Math.sin(d/R)*Math.cos(φ1), Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ2));
-
-    array = [[radToDeg(λ2), radToDeg(φ2)], [startLongitude, 0], [radToDeg(λ2d), -radToDeg(φ2)]]
-    return makeGeoObject(array);
+    var φ2 = Math.asin(Math.sin(d/R)*Math.cos(i1));
+    var λ2 = Math.atan2(Math.sin(i1)*Math.sin(d/R), Math.cos(d/R));
+    return [λ2, φ2];
 };
 
 
 // creates the orbital path line object for a given satellite
-function makeGeoObject(positionArray) {
-        var object = {
-            "type":"Feature",
-            "geometry":{
-                "type":"LineString",
-                "coordinates": positionArray
-            }
-        };
-        return object;
-}
+// function makeGeoObject(positionArray) {
+//         var object = {
+//             "type":"Feature",
+//             "geometry":{
+//                 "type":"LineString",
+//                 "coordinates": positionArray
+//             }
+//         };
+//         return object;
+// }
 
 
-// random position along 'equator' of the satellite path
+// random position along 'equator' of the individual satellite path
 function makeSatelliteObject() {
     var object = {
         "type":"Feature",
@@ -79,4 +92,9 @@ function degToRad(deg) {
 }
 function radToDeg(rad) {
     return rad*180/Math.PI;
+}
+
+// random range for opacity
+function randOpacity(min, max) {
+  return Math.random() * (max - min) + min;
 }
